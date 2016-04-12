aa627af04810d78b615fa41e2393bf12185503ab
Eric Schulte <Eric@EricSchultePro-4.local>
2016-04-11 23:13:55 -0400

    authority test wip

--- test/ParticipantAuthority.js
+++ test/ParticipantAuthority.js
@@ -1,22 +1,35 @@
 const Botswana = {name: "Botswana"};
 const Authority = {name: "Ministry of Minerals, Energy and Water Resources"};
 
-
 contract('KPCS', function(accounts) {
   it("Register as a Participant Authority", function(done) {
   	var kpcs;
-  	var participant;
   	KPCS.new({from: accounts[0]}).then(
   		function (instance) {
   			kpcs = instance;
-  			console.log(kpcs);
   		}
   	).then(
   		function() {
-  			return ParticipantAuthority.new(Authority.name, accounts[0], {from: accounts[1]});
+  			return Participant.new(Botswana.name, accounts[0], {from: accounts[1]});
   		}
   	).then(
-  		function() {
+  		function(participant) {
+  			Botswana.instance = participant;
+  			return ParticipantAuthority.new(Authority.name, Botswana.instance.administrator, {from: accounts[2]});
+  		}
+  	).then(
+  		function(authority) {
+  			Authority.instance = authority;
+  			return Authority.instance.name.call();
+  		}
+  	).then(
+  		function(name) {
+  			assert.equal(name, Authority.name);
+  			return Authority.instance.administrator.call();
+  		}
+  	).then(
+  		function (administrator) {
+  			assert.equal(administrator, Botswana.instance.address)
   			done();
   		}
   	).catch({
